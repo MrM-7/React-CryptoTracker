140b4cf28e632cc364b7a45846061657ec414ba0
Mehul <rmmehul7@gmail.com>
2022-11-27 09:44:18 +0530

    second commit

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Crypto Tracker
           </Typography>
           <Typography
             variant="subtitle2"
@@ -51,7 +51,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Get all the Info regarding your favorite Crypto Currency
+            Janiye  cryptocurrency ko kareeb se 
           </Typography>
         </div>
         <Carousel />
